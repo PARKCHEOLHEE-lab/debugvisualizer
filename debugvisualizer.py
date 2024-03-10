e6598b59269d8a841287dab4be3045ecbe54d8e3
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-03-10 20:06:39 +0900

    hook

--- debugvisualizer.py
+++ debugvisualizer.py
@@ -1,4 +1,3 @@
-
 import json
 import trimesh
 import numpy as np
@@ -16,7 +15,7 @@ class Plotter:
         self.map_z_to_y = map_z_to_y
         self.orthographic = orthographic
         self.__gen_geometries_dict(geometries)
-    
+
     @property
     def geometries_data(self):
         return self.__geometries_data
@@ -44,11 +43,13 @@ class Plotter:
                 }
             },
         }
-        
+
         if self.orthographic:
             self.__viz_dict["layout"]["scene"]["camera"] = dict(projection=dict(type="orthographic"))
 
-    def __get_geometry_data(self, geometry: Union[trimesh.Trimesh, Polygon, LineString, MultiPolygon, MultiLineString, Point]) -> dict:
+    def __get_geometry_data(
+        self, geometry: Union[trimesh.Trimesh, Polygon, LineString, MultiPolygon, MultiLineString, Point]
+    ) -> dict:
         """get plotly format data"""
 
         data = {
@@ -57,7 +58,7 @@ class Plotter:
             "z": [],
             "type": "mesh3d" if isinstance(geometry, trimesh.Trimesh) else "scatter3d",
             "name": "mesh" if isinstance(geometry, trimesh.Trimesh) else "geometry",
-            "showlegend": True
+            "showlegend": True,
         }
 
         if isinstance(geometry, trimesh.Trimesh):
@@ -113,7 +114,7 @@ class Plotter:
 
         if geometry.has_z:
             z = list(np.array(coords)[:, 2])
-        
+
         return x, y, z
 
     def visualize(self, to_json: bool = True) -> dict:
@@ -121,53 +122,54 @@ class Plotter:
 
         if to_json:
             return json.dumps(self.viz_dict)
-        
+
         return self.viz_dict
-    
+
     def save(self, filename: str = "visualization.html") -> None:
         """save the visualized result to an HTML file."""
 
-        fig = go.Figure(data=[go.Mesh3d(**geom) if geom["type"] == "mesh3d" else go.Scatter3d(**geom) for geom in self.geometries_data])
+        fig = go.Figure(
+            data=[
+                go.Mesh3d(**geom) if geom["type"] == "mesh3d" else go.Scatter3d(**geom) for geom in self.geometries_data
+            ]
+        )
         fig.update_layout(**self.viz_dict["layout"])
-        
-        plot(fig, filename=filename, auto_open=False)
-        
 
+        plot(fig, filename=filename, auto_open=False)
 
 
 if __name__ == "__main__":
-    polygon = Polygon([[0,0], [2,0], [2,2], [0,2], [0,0]])
-    polygon2 = Polygon([[1,1], [2,1], [2,2], [1,2], [1,1]])
-    polygon3 = Polygon([[3,3], [3,5], [5,5], [5,3], [3,3]])
+    polygon = Polygon([[0, 0], [2, 0], [2, 2], [0, 2], [0, 0]])
+    polygon2 = Polygon([[1, 1], [2, 1], [2, 2], [1, 2], [1, 1]])
+    polygon3 = Polygon([[3, 3], [3, 5], [5, 5], [5, 3], [3, 3]])
     multipolygon = MultiPolygon([polygon, polygon3])
 
-    l1 = LineString([[0,0], [1,1]])
-    l2 = LineString([[2,2], [3,3]])
+    l1 = LineString([[0, 0], [1, 1]])
+    l2 = LineString([[2, 2], [3, 3]])
     multilinestring = MultiLineString([l1, l2])
 
-    p1 = Point(2,2)
+    p1 = Point(2, 2)
     ep = Point()
-    
-    mesh_vertices = np.array([
-        [0, 0, 0],
-        [1, 0, 0],
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-        [1, 0, 1],
-        [1, 1, 1],
-        [0, 1, 1] 
-    ])
 
-    mesh_faces = np.array([
-        [0, 1, 2], [0, 2, 3],
-        [4, 5, 6], [4, 6, 7],
-        [0, 1, 5], [0, 5, 4],
-        [1, 2, 6], [1, 6, 5],
-        [2, 3, 7], [2, 7, 6],
-        [3, 0, 4], [3, 4, 7] 
-    ])
+    mesh_vertices = np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 0, 1], [1, 0, 1], [1, 1, 1], [0, 1, 1]])
+
+    mesh_faces = np.array(
+        [
+            [0, 1, 2],
+            [0, 2, 3],
+            [4, 5, 6],
+            [4, 6, 7],
+            [0, 1, 5],
+            [0, 5, 4],
+            [1, 2, 6],
+            [1, 6, 5],
+            [2, 3, 7],
+            [2, 7, 6],
+            [3, 0, 4],
+            [3, 4, 7],
+        ]
+    )
 
     mesh = trimesh.Trimesh(vertices=mesh_vertices, faces=mesh_faces)
-    
-    a=1
+
+    a = 1
