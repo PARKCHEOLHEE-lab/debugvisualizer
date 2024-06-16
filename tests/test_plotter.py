8aed4ab97c06b6edc0fa87bafe967a45464605a6
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-06-16 15:46:34 +0900

    pytest

---
+++ tests/test_plotter.py
@@ -0,0 +1,35 @@
+import json
+import pytest
+
+from debugvisualizer import Plotter
+
+
+@pytest.fixture
+def plotter_dummy() -> Plotter:
+    return Plotter()
+
+
+def test_visualize_to_json(plotter_dummy: Plotter) -> None:
+    """Tests `visualize` method with `to_json=True` whether it returns valid result."""
+
+    visualized_str = plotter_dummy.visualize(to_json=True)
+    visualied_dict = json.loads(visualized_str)
+
+    assert isinstance(visualized_str, str)
+    assert isinstance(visualied_dict, dict)
+    assert "kind" in visualied_dict
+    assert "data" in visualied_dict
+    assert "layout" in visualied_dict
+
+
+def test_visualize_to_dict(plotter_dummy: Plotter) -> None:
+    """Tests `visualize` method with `to_json=False` whether it returns valid result."""
+
+    visualized_dict = plotter_dummy.visualize(to_json=False)
+    visualized_str = json.dumps(visualized_dict)
+
+    assert isinstance(visualized_dict, dict)
+    assert isinstance(visualized_str, str)
+    assert "kind" in visualized_dict
+    assert "data" in visualized_dict
+    assert "layout" in visualized_dict
